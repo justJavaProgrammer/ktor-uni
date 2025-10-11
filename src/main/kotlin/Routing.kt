ae90dfc3d9ed82f19c9b27e15265bd9948a49786
odeyalo <theprogrammerinfuture@gmail.com>
2025-10-11 22:25:38 +0300

    Add new endpoint to fetch all tasks

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -1,13 +1,18 @@
 package com.odeyalo
 
+import io.ktor.http.ContentType
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import com.odeyalo.model.*
 
 fun Application.configureRouting() {
     routing {
-        get("/") {
-            call.respondText("Hello World!")
+        get("/tasks") {
+            call.respondText(
+                contentType = ContentType.parse("text/html"),
+                text = tasks.tasksAsTable()
+            )
         }
     }
 }
