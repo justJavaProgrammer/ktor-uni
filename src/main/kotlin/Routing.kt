eacf1d709d2bd5b47a4335db7a50e7f032827bd4
odeyalo <theprogrammerinfuture@gmail.com>
2025-10-11 22:58:16 +0300

    Add endpoint to fetch the tasks by priority

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -5,6 +5,7 @@ import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import com.odeyalo.model.*
+import io.ktor.http.HttpStatusCode
 
 fun Application.configureRouting() {
     routing {
@@ -14,6 +15,30 @@ fun Application.configureRouting() {
                 contentType = ContentType.parse("text/html"),
                 text = tasks.tasksAsTable()
             )
+        }
+        get("/tasks/byPriority/{priority?}") {
+            val priorityAsText = call.parameters["priority"]
+            if (priorityAsText == null) {
+                call.respond(HttpStatusCode.BadRequest)
+                return@get
+            }
+
+            try {
+                val priority = Priority.valueOf(priorityAsText)
+                val tasks = TaskRepository.tasksByPriority(priority)
+
+                if (tasks.isEmpty()) {
+                    call.respond(HttpStatusCode.NotFound)
+                    return@get
+                }
+
+                call.respondText(
+                    contentType = ContentType.parse("text/html"),
+                    text = tasks.tasksAsTable()
+                )
+            } catch(ex: IllegalArgumentException) {
+                call.respond(HttpStatusCode.BadRequest)
+            }
         }
     }
 }
